557adaba15b538ceee3c31fb6c9354a4d80e7742
Dan Morin <dan.morin@gmail.com>
2012-05-15 15:52:21 -0400

    Cleaned up code. Removed 'Buy gift card' link override. Sending via state machine instead of method chaining.

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -1,53 +1,55 @@
-class Spree::GiftCard < ActiveRecord::Base
-  belongs_to :variant
-  belongs_to :line_item
-  belongs_to :sender, :class_name => "User"
-  belongs_to :recipient, :class_name => "User"
-  belongs_to :store_credit
+module Spree
+  class GiftCard < ActiveRecord::Base
+    belongs_to :variant
+    belongs_to :line_item
+    belongs_to :sender, :class_name => "User"
+    belongs_to :recipient, :class_name => "User"
+    belongs_to :store_credit
 
-  validates :email, :presence => true
-  validates :name, :presence => true
-  validates :sender_name, :presence => true
-  validates :variant, :presence => {:message => "Price option must be selected"}
+    validates :email, :presence => true
+    validates :name, :presence => true
+    validates :sender_name, :presence => true
+    validates :variant, :presence => {:message => "Price option must be selected"}
 
-  before_create :generate_token
+    before_create :generate_token
 
-  attr_accessible :name, :email, :sender_name, :note, :variant_id, :delivery_method, :cc_me
+    attr_accessible :name, :email, :sender_name, :note, :variant_id, :delivery_method, :cc_me
 
-  scope :users_cards, lambda { |user_id| 
-    where("(sender_id = ? OR recipient_id = ?) AND sent_at IS NOT NULL", user_id, user_id).
-    order('created_at desc')  
-  }
+    scope :users_cards, lambda { |user_id| 
+      where("(sender_id = ? OR recipient_id = ?) AND sent_at IS NOT NULL", user_id, user_id).
+      order('created_at desc')  
+    }
   
-  def self.remaining_credit(user_id)
-     joins(:store_credit).where("recipient_id = ?", user_id).sum(:remaining_amount)
-  end
+    def self.remaining_credit(user_id)
+       joins(:store_credit).where("recipient_id = ?", user_id).sum(:remaining_amount)
+    end
 
-  def price
-    # self.line_item || self.variant ? (self.line_item ? self.line_item.price * self.line_item.quantity : self.variant.price) : 0.0
+    def price
+      # self.line_item || self.variant ? (self.line_item ? self.line_item.price * self.line_item.quantity : self.variant.price) : 0.0
     
-    if line_item
-      line_item.price * line_item.quantity
-    elsif variant
-      variant.price
-    else
-      0.0
+      if line_item
+        line_item.price * line_item.quantity
+      elsif variant
+        variant.price
+      else
+        0.0
+      end
+    end
+
+    def register(user)
+      @sc = StoreCredit.create(:amount => price, :remaining_amount => price,
+                                  :reason => 'gift card', :user => user)
+    
+      self.is_received      = true
+      self.recipient        = user
+      self.store_credit_id  = @sc.id
+      self.save
+    end
+
+    private
+
+    def generate_token
+      self.token = Digest::SHA1.hexdigest([Time.now, rand].join)
     end
   end
-
-  def register(user)
-    @sc = StoreCredit.create(:amount => price, :remaining_amount => price,
-                                :reason => 'gift card', :user => user)
-    
-    self.is_received      = true
-    self.recipient        = user
-    self.store_credit_id  = @sc.id
-    self.save
-  end
-
-  private
-
-	def generate_token
-	  self.token = Digest::SHA1.hexdigest([Time.now, rand].join)
-	end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,17 +1,21 @@
-Spree::Order.class_eval do
-  # Finalizes an in progress order after checkout is complete.
-  # Called after transition to complete state when payments will have been processed
-  def finalize_with_gift_card!
-    finalize_without_gift_card!
-    self.line_items.each do |li|
-      OrderMailer.gift_card_email(li.gift_card, self).deliver if li.gift_card && li.gift_card.delivery_method == 'email'
+module Spree
+  Order.class_eval do
+  
+    Spree::Order.state_machines[:state].after_transition :to => 'complete', :do => :finalize_with_gift_card!
+  
+    # Finalizes an in progress order after checkout is complete.
+    # Called after transition to complete state when payments will have been processed
+    def finalize_with_gift_card!
+      self.line_items.each do |li|
+        if li.gift_card && li.gift_card.delivery_method == 'email'
+          OrderMailer.gift_card_email(li.gift_card, self).deliver 
+        end
+      end
+    end
+
+    def contains?(variant)
+      return false if variant.product.is_gift_card?
+      line_items.detect{|line_item| line_item.variant_id == variant.id}
     end
   end
-  alias_method_chain :finalize!, :gift_card
-
-  def contains?(variant)
-    return false if variant.product.is_gift_card?
-    line_items.detect{|line_item| line_item.variant_id == variant.id}
-  end
-
 end
