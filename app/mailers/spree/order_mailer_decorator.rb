1aa31b30297a38cd4d430e8d3c0841d5136f0609
Dan Morin <dan.morin@gmail.com>
2012-05-18 20:41:08 -0400

    Removed debugging and only setting sent at if not already set

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -2,14 +2,9 @@ Spree::OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
-    logger.info '********'
-    logger.info card.sender || '***'
-    logger.info card.sender.email || '***'
-    logger.info card.cc_me || '***'
-    logger.info '********'
-    cc = (card.sender.email if card.cc_me) || ''
-    subject = t('spree_gift_card.messages.email_subject', :site => Spree::Config[:site_url], :sender => card.sender_name)
-    @gift_card.update_attribute(:sent_at, Time.now)
+    cc = (card.cc_me && card.sender) ? card.sender.email : []
+    subject = t('spree_gift_card.messages.email_subject', :sender => card.sender_name)
+    @gift_card.update_attribute(:sent_at, Time.now) unless @gift_card.sent_at?
     mail(:to => card.email, :cc => cc, :subject => subject)
   end
 end
